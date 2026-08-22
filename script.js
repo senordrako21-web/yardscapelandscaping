b072d8951160dc78131b8f0731a45d67f47e5012
senordrako21-web <senordrako21@gmail.com>
2026-08-22 00:01:33 -0500

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('.menu-toggle')?.addEventListener('click',()=>document.querySelector('.main-nav')?.classList.toggle('open'));
-document.querySelectorAll('.main-nav a').forEach(a=>a.addEventListener('click',()=>document.querySelector('.main-nav')?.classList.remove('open')));

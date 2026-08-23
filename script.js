52cc85efea607eafc4baef8b2be98404f6c9619f
senordrako21-web <senordrako21@gmail.com>
2026-08-23 00:16:42 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.menu-toggle')?.addEventListener('click', () => {
-  const nav = document.querySelector('.nav nav');
-  if (nav) nav.style.display = nav.style.display === 'flex' ? '' : 'flex';
-});
